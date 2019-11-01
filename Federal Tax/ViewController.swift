468a7ab8590f55317df5a131c1c2a3b0ee5c28e5
Scott Doggett <sdoggett@lcs.on.ca>
2019-10-31 21:06:35 -0400

    Initial Commit

---
+++ Federal Tax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Federal Tax
+//
+//  Created by Doggett, Scott on 2019-10-31.
+//  Copyright © 2019 Doggett, Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
