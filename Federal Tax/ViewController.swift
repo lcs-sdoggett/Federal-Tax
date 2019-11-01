16671455e1e563c3229c830c41b50303ab0b8a03
Scott Doggett <sdoggett@lcs.on.ca>
2019-10-31 21:36:13 -0400

    finished converting strings

--- Federal Tax/ViewController.swift
+++ Federal Tax/ViewController.swift
@@ -9,7 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: PROPERTIES
+    
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var incomeLabel: UITextField!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    // MARK: METHODS
 
+    // When "Calculate Tax Owing" button is pressed
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        // Convert Name to string
+         guard let nameAsString = nameLabel.text else {
+                   bottomLabel.text = "Enter a Name"
+                   return
+               }
+        
+        // Convert income to double
+        guard let incomeAsString = incomeLabel.text else {
+                   bottomLabel.text = "Enter an Income"
+                   return
+               }
+        // Continue
+        guard let incomeAsDouble = Double(incomeAsString) else {
+            bottomLabel.text = "Enter a Number"
+            return
+        }
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
